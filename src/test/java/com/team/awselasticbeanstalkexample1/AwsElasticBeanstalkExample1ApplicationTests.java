992db8f156528fe3714ffc39574990458e49daa5
Shabana  R <shabanar@cloudthat.com>
2022-01-05 19:26:24 +0530

    AWS Beanstalk code

---
+++ src/test/java/com/team/awselasticbeanstalkexample1/AwsElasticBeanstalkExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.awselasticbeanstalkexample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstalkExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
