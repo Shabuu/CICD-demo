1cbd1cc04dd34c75c165d4297f949629179f304c
Shabana  R <shabanar@cloudthat.com>
2022-01-06 14:16:21 +0530

    chnaged hello.java

--- src/main/java/com/team/awselasticbeanstalkexample1/HelloController.java
+++ src/main/java/com/team/awselasticbeanstalkexample1/HelloController.java
@@ -1,13 +1,15 @@
 package com.team.awselasticbeanstalkexample1;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
-
 public class HelloController {
+
+    @GetMapping
     public String hello() {
-        return "Hello All";
-    } 
+        return "Hello TechPrimers";
+    }
 }
